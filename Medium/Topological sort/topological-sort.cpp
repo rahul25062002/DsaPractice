a9ea3e6f1475993ee55465ee31c6617584a37116
rahul25062002 <75932347+rahul25062002@users.noreply.github.com>
2023-06-01 05:28:15 +0530

    Updated Solution - GfG to GitHub

--- Medium/Topological sort/topological-sort.cpp
+++ Medium/Topological sort/topological-sort.cpp
@@ -4,34 +4,38 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution
-{    private:
-       void dfs(int node,int vis[],vector<int> adj[],stack<int> &st){
-           vis[node]=1;
-           for(auto it:adj[node]){
-               if(!vis[it]) dfs(it,vis,adj,st);
-           }
-           st.push(node);
-       }
+{    void dfs( vector<int> adj[],stack<int> &st,
+	    vector<int> &vis,int index){
+	      vis[index]  =1;
+	      for(auto it:adj[index]){
+	          if(!vis[it]){
+	              dfs(adj,st,vis,it);
+	          }
+	      }
+	      st.push(index);
+	      return ;
     
+}
 	public:
 	//Function to return list containing vertices in Topological order. 
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
-	    int vis[V]={0};
 	    stack<int> st;
+	    vector<int> vis(V,0);
 	    for(int i=0;i<V;i++){
 	        if(!vis[i]){
-	            dfs(i,vis,adj,st);
+	           dfs(adj,st,vis,i);  
 	        }
 	    }
-	    vector<int> ans;
+	    vector<int> topo;
 	    while(!st.empty()){
-	        ans.push_back(st.top());
+	        int i=st.top();
 	        st.pop();
+	        topo.push_back(i);
 	    }
-	    return ans;
-	   
+	    
+	    return topo;
 	}
 };
 
