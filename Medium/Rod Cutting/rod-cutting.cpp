60c7878433247db96ffd2be028515b9d8214ab2f
rahul25062002 <75932347+rahul25062002@users.noreply.github.com>
2023-07-19 17:56:02 +0530

    Updated Solution - GfG to GitHub

--- Medium/Rod Cutting/rod-cutting.cpp
+++ Medium/Rod Cutting/rod-cutting.cpp
@@ -9,16 +9,25 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
-  public:
-    int cutRod(int price[], int N) {
-     vector<int> dp(N + 1, 0);
-    for (int i = 1; i <= N; i++) {
-        for (int j = 0; j < i; j++) {
-            dp[i] = max(dp[i], price[j] + dp[i - j-1]);
-            
-        }
+  private:
+    int fun(int index,int left,int price[],int n,vector<vector<int>> &dp){
+        //base case
+        if(left==0) return 0;
+        if(left<index+1) return 0;
+        if(index>=n) return 0;
+        if(dp[index][left]!=-1) return dp[index][left];
+        int nottaken=fun(index+1,left,price,n,dp);
+        int taken=0;
+        if(left>=index+1) taken=price[index]+fun(index,left-index-1,price,n,dp);
+        return dp[index][left]=max(taken,nottaken);
+        
     }
-    return dp[N];}
+  public:
+    int cutRod(int price[], int n) {
+        //code here
+        vector<vector<int>> dp(n,vector<int>(n+1,-1));
+        return fun(0,n,price,n,dp);
+    }
 };
 
 //{ Driver Code Starts.
